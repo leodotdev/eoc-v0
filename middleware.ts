baeb9d32c20cf85d92b6dfd6f463c94b569e14e9
Leo SF <leo@leo.miami>
2025-04-03 14:44:25 -0400

    Add middleware to apply CSP headers in development

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // Get response
+  const response = NextResponse.next();
+
+  // Add CSP header
+  response.headers.set(
+    'Content-Security-Policy',
+    `default-src 'self'; 
+     script-src 'self' 'unsafe-inline' 'unsafe-eval'; 
+     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; 
+     font-src 'self' https://fonts.gstatic.com; 
+     img-src 'self' data: https:; 
+     frame-src https://www.youtube-nocookie.com https://www.youtube.com; 
+     connect-src 'self' https://*.vercel.app`
+  );
+
+  return response;
+}
+
+// Only run middleware on your page routes
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
