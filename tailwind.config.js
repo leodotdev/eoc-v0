9b7a827034c417d15b9b9b554f8dae5c8d40cfa0
Leo SF <leo@leo.miami>
2025-04-03 10:43:27 -0400

    style: improve container padding for better responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
